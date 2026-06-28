8891224ac9c877dab3248e856fe39e793a6e90fc
nayansuva03 <nayan.suva.ahir.0369@gmail.com>
2026-06-28 22:44:19 +0530

    cors origin config

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express();
 
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://mcqs-generator-ruby.vercel.app/"
+}));
 app.use(express.json());
 app.use("/api/generate", generateRoute);
 
